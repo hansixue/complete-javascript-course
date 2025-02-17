a2a4e1c41e77ba291ac75377c5a291f47538d46c
hansixue <hansixue@gmail.com>
2025-02-17 16:17:20 +0900

    all

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const bookings = [];
+
+const creatbooking = function(flightNum,numPperson=1,price=100){
+
+    const booking ={
+	flightNum,
+	numPperson,
+	price
+    }
+    
+    console.log(booking);
+    bookings.push(booking);
+}
+
+creatbooking("lm123");
